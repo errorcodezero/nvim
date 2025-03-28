8e0ff232ad1968a8cf5f3ee4a2db90fdee8b8be9
ErrorCode0 <me@errorcodezero.dev>
2025-03-28 00:00:28 -0700

    im not even sure

--- lua/errorcodezero/plugins/alpha.lua
+++ lua/errorcodezero/plugins/alpha.lua
@@ -3,6 +3,14 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local dashboard = require("alpha.themes.dashboard")
+		local quotes = {
+			"Object oriented programs are offered as alternatives to correct ones - Dijkstra",
+			"Java: Write once, debug everywhere",
+			"Nothing is more permanent than a temporary solution.",
+			"There are 2 hard problems in computer science: cache invalidation, naming things, and off-by-1 errors.",
+			"Rise and shine Mr. Freeman",
+			"Postfix vs Prefix what is the difference?",
+		}
 		dashboard.section.header.val = {
 			"███████╗██████╗░██████╗░░█████╗░██████╗░░█████╗░░█████╗░██████╗░███████╗░█████╗░",
 			"██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗",
@@ -18,6 +26,8 @@ return {
 			dashboard.button("4", "󰒋 LSP Servers", ":Mason<enter>"),
 			dashboard.button("5", "󰅖 Quit", ":q!<enter>"),
 		}
+		math.randomseed(os.time())
+		dashboard.section.footer.val = quotes[math.random(#quotes)]
 		require("alpha").setup(dashboard.config)
 	end,
 }
